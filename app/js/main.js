f8d2768fede71d5b4aabfcb45ff9dd01a8e8ef9e
Plaha-ua <dv.plakhotin@gmail.com>
2020-12-18 23:54:47 +0200

    added 1 and 2 filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 $ (function (){
+
+$('.filter-price__input').ionRangeSlider({
+  type:"double",
+  prefix: "$",
+  onStart: function (data){
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+  onChange: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+        },
+});
+
+
 $('.top-slider__inner').slick({
   dots:true,
   arrows:false,
